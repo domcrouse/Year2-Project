85c108d8f86a86037fc5e833ec5a9cc735e9c57a
Silenthowler <23553428+domcrouse@users.noreply.github.com>
2019-09-30 13:00:34 +0100

    Chapter2

---
+++ Year2-Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController charController;
+
+    [SerializeField] float jumpSpeed = 20.0f;
+    [SerializeField] float gravity = 1.0f;
+    float yVelocity = 0.0f;
+
+    [SerializeField] float moveSpeed = 5.0f;
+    public float h;
+    public float v;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis ("Horizontal");
+        v = Input.GetAxis ("Vertical");
+
+        Vector3 direction = new Vector3 (h, 0, v);
+        Vector3 velocity = direction * moveSpeed;
+
+        if(charController.isGrounded){
+            if(Input.GetButtonDown("Jump"))
+            {
+                yVelocity = jumpSpeed;
+            }else{
+                yVelocity-= gravity;
+            }
+            velocity.y = yVelocity;
+            
+            velocity = transform.TransformDirection (velocity);
+
+            charController.Move(velocity*Time.deltaTime);
+        }
+    }
+}
